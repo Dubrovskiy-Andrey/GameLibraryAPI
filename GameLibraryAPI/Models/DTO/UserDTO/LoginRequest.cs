99da7172be34056138cce67c458f50c7bb39c70e
Dubrovskiy-Andrey <nataladubrovskaa38724@gmail.com>
2025-11-21 14:05:47 +0500

    убрал roleid

--- GameLibraryAPI/Models/DTO/UserDTO/LoginRequest.cs
+++ GameLibraryAPI/Models/DTO/UserDTO/LoginRequest.cs
@@ -9,6 +9,6 @@ namespace GameLibraryAPI.Models.DTO.UserDTO
         [Required(ErrorMessage = "необходимо ввести пароль")]
         [MinLength(8, ErrorMessage = "минимальная длина пароля 8 символов")]
         public string Password { get; set; } = null!;
-        public int RoleId { get; set; }
+        //public int RoleId { get; set; }
     }
 }
